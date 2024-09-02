8557c30acffe705fc569e21111cea89da0a8e10c
ChtiouiMoncer <moncercht@gmail.com>
2024-09-02 17:19:37 +0100

    Metada changes in the homepage

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,14 @@
 import "./globals.css";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Home Page",
+  description: "Home Page description",
+  viewport: "width=device-width, initial-scale=1",
+  icons: {
+    icon: "/favicon.ico",
+  },
+};
 
 export default function RootLayout({ children }) {
   return (
